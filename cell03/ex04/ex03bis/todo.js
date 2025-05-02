6cb73e0f6c9da2b4f45a019f4c373fccaf4e6df6
Noon429 <66011273@kmitl.ac.th>
2025-05-02 11:44:44 +0700

    Update todo.js

--- cell03/ex04/ex03bis/todo.js
+++ cell03/ex04/ex03bis/todo.js
@@ -1,22 +1,16 @@
 let itemCount = 0;
 
-// Utility: Get cookie by name
 function getCookie(name) {
-  const cookies = document.cookie.split('; ');
-  for (const cookie of cookies) {
-    const [key, val] = cookie.split('=');
-    if (key === name) return decodeURIComponent(val);
-  }
-  return null;
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return decodeURIComponent(parts.pop().split(';').shift());
 }
 
-// Utility: Set cookie
 function setCookie(name, value, days = 365) {
   const expires = new Date(Date.now() + days * 864e5).toUTCString();
   document.cookie = `${name}=${encodeURIComponent(value)}; expires=${expires}; path=/`;
 }
 
-// Save list items to cookie
 function saveTodoList() {
   const todos = [];
   $(".todo-item").each(function () {
@@ -25,47 +19,43 @@ function saveTodoList() {
   setCookie("todoList", JSON.stringify(todos));
 }
 
-// Load list from cookie
 function loadTodoList() {
   const todoList = getCookie("todoList");
   if (todoList) {
-    try {
-      const todos = JSON.parse(todoList);
-      todos.reverse().forEach(todo => addItem(todo, false)); // maintain insertion order
-    } catch (e) {
-      console.error("Invalid cookie data:", e);
-    }
+    const todos = JSON.parse(todoList);
+    todos.reverse().forEach(todo => addItem(todo, false));
   }
 }
 
-// Add item to the DOM
 function addItem(text = null, save = true) {
   const input = $("#textfield");
   const todoText = text || input.val().trim();
   if (todoText === "") return;
 
   itemCount++;
+  const todoItem = $(`<div class="todo-item" id="todo-${itemCount}"></div>`);
+  const textNode = document.createTextNode(todoText);
+  const removeBtn = $("<button>Remove</button>");
 
-  const $todoItem = $("<div></div>")
-    .addClass("todo-item")
-    .attr("id", `todo-${itemCount}`)
-    .text(todoText);
-
-  const $removeBtn = $("<button>Remove</button>").on("click", function () {
-    $todoItem.remove();
-    saveTodoList();
+  removeBtn.on("click", function () {
+    const confirmation = confirm("Are you sure you want to remove this TO DO?");
+    if (confirmation) {
+      todoItem.remove();
+      saveTodoList();
+    }
   });
 
-  $todoItem.append($removeBtn);
-  $("#todoList").prepend($todoItem);
+  todoItem.append(textNode, removeBtn);
+  $("#todoList").prepend(todoItem);
 
   if (!text) input.val("");
-
   if (save) saveTodoList();
 }
 
-// On page load
 $(document).ready(function () {
-  $("#addbtn").on("click", () => addItem());
+  $("#addbtn").on("click", function () {
+    addItem();
+  });
+
   loadTodoList();
 });
